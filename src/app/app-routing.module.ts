5df32fa1d0d5f2cb71491504aaffb782d09d9b7d
NoAccount1 <74969657+NoAccount1@users.noreply.github.com>
2021-02-03 13:28:08 +0100

    Creating game.component & game-list.component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { GameListComponent } from './game-list/game-list.component';
+import { HomeComponent } from './home/home.component';
+import { GameComponent } from './game/game.component';
+import { ErrorComponent } from './error/error.component';
+
+const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'games', component: GameListComponent },
+    { path: 'games/:game', component: GameComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: false })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GameComponent } from './game/game.component';
+import { HomeComponent } from './home/home.component';
+import { GameListComponent } from './game-list/game-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialLiteModule } from './material/material-lite.module';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { ErrorComponent } from './error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent,
+    HomeComponent,
+    GameListComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialLiteModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
