7932b9122bd9f11dda4fdfc19f8624ce9ac51cda
NoAccount1 <mouamem2edunom@gmail.com>
2022-04-05 17:24:32 +0200

    Fix random stuffs and break some others

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ViewChildren, QueryList } from '@angular/core';
 import { FooterComponent, FooterModule } from './common/footer.component';
+import { ItemComponent } from './common/item/item.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-
 export class AppComponent implements OnInit {
+
+  @ViewChildren(ItemComponent) appitem: QueryList<ItemComponent> | undefined;
+  @Input() fullView: boolean | undefined;
 
   mobile = false;
 
-  ngOnInit(): void {  }
+  getChildTitle(fullView: string): void{
+    console.log('Received child\'s title: ' + fullView);
+  }
+  ngOnInit(): void {
+    console.log(`app.component  : ${this.fullView}`);
+  }
 }
--- src/app/common/item/item.component.ts
+++ src/app/common/item/item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { GAMES } from '../../games';
 import { Location } from '@angular/common';
@@ -6,27 +6,32 @@ import { Location } from '@angular/common';
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
-  styleUrls: ['./item.component.scss']
+  styleUrls: ['./item.component.scss'],
 })
 export class ItemComponent implements OnInit {
+  @Output() notify: EventEmitter<boolean | undefined> = new EventEmitter<boolean | undefined>();
 
   @Input() mainGame: any;
   @Input() fullView: boolean | undefined;
+
+  gameUrl = this.activatedRoute.snapshot.params.game;
 
   constructor(
     public activatedRoute: ActivatedRoute,
     public _location: Location // tslint:disable-line: variable-name
   ) {}
 
-  gameUrl = this.activatedRoute.snapshot.params.game;
   backPage = () => {
     this._location.back();
   }
+  sendTitle(): void {
+    this.notify.emit(this.fullView);
+  }
+
 
   ngOnInit(): void {
-    if (this.fullView !== false) {
-      this.fullView = true;
-    }
+    if (this.fullView !== false) { this.fullView = true; }
+    console.log(`item.component  : ${this.fullView}`);
     const gameList = GAMES;
     const gameUrl: string = this.activatedRoute.snapshot.params.game;
     const path: string = this.activatedRoute.snapshot.url.toString();
